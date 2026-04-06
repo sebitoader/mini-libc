affc3019e9a038d4b8c8f3c79b61e3b7f0b2c5f7
sebitoader <152149681+sebitoader@users.noreply.github.com>
2026-04-06 12:52:49 +0300

    Update truncate.c

--- io/truncate.c
+++ io/truncate.c
@@ -6,12 +6,10 @@
 
 int truncate(const char *path, off_t length)
 {
-	/* TODO: Implement truncate(). */
 	int ret = syscall(__NR_truncate, path, length);
 	if (ret < 0) {
 		errno = -ret;
 		return -1;
 	}
 	return ret;
-	//return -1;
 }
