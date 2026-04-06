36cacc47e7025c6aef52067ae3d853dc3dc280a9
sebitoader <152149681+sebitoader@users.noreply.github.com>
2026-04-06 12:51:10 +0300

    Update lseek.c

--- io/lseek.c
+++ io/lseek.c
@@ -6,12 +6,10 @@
 
 off_t lseek(int fd, off_t offset, int whence)
 {
-	/* TODO: Implement lseek(). */
 	int ret = syscall(__NR_lseek, fd, offset, whence);
 	if (ret < 0) {
 		errno = -ret;
 		return -1;
 	}
 	return ret;
-	//return -1;
 }
