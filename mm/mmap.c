84f9e935f15c6a89533240c0d4aa613aa555ba2e
sebitoader <152149681+sebitoader@users.noreply.github.com>
2026-04-06 12:55:26 +0300

    Update mmap.c
    Implemented mremap and munmap system calls.

--- mm/mmap.c
+++ mm/mmap.c
@@ -16,7 +16,6 @@ void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	/* TODO: Implement mremap(). */
 	long result = syscall(__NR_mremap, (long)old_address, old_size, new_size, flags);
     if (result < 0) {
         errno = -result;
@@ -27,7 +26,6 @@ void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 
 int munmap(void *addr, size_t length)
 {
-	/* TODO: Implement munmap(). */
 	long result = syscall(__NR_munmap, (long)addr, length);
     if (result < 0) {
         errno = -result;
